e6976f2a43dc85bf9f1c3083cc11a43e89b805ac
Achmad Faishal Fadhil <AchmadFaishal.Fadhil@outlook.com>
2022-06-02 21:27:39 +0700

    add players

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -14,6 +15,13 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <h1 className={styles.title}>
+          Kalau mau liat Players, klik ini 
+          <Link href="/players">
+            <a>KLIK DISINI</a>
+          </Link>
         </h1>
 
         <p className={styles.description}>
